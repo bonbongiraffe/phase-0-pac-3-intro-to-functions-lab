e9190269046806f2ef01a7081b81db26e205ac0b
bonbongiraffe <francescowai@yahoo.com>
2023-05-01 15:03:35 -0400

    "first attempt"

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string){
+    return string.toUpperCase();
+}
+
+function whisper(string){
+    return string.ToLowerCase();
+}
+
+function logShout(string){
+    console.log(shout(string));
+}
+
+function logWhisper(string){
+    console.log(whisper(string));
+}
+
+function sayHiToHeadphonedRoommate(string){
+    if (string.ToLowerCase != string)
+        return "YES INDEED!";
+    if (string.toUpperCase != string)
+        return "I can't hear you!";
+    if (string = "Let's have dinner together!")
+        return "I would love to!";
+    else 
+        return '';
+}
